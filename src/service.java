5e581e2ad3573611bac9cde8793b6062d6ae231c
MattK <mateo2021.tablet1@gmail.com>
2023-08-17 19:56:07 -0500

    confirmado

---
+++ src/service.java
@@ -0,0 +1,15 @@
+public class service {
+    private int sum;
+
+    public service(int sum) {
+        this.sum = sum;
+    }
+
+    public int getSum() {
+        return sum;
+    }
+
+    public void setSum(int sum) {
+        this.sum = sum;
+    }
+}
